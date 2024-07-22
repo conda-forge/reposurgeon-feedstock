d6632af29edb05a640a4af60dfffd7af296889a9
Daniel Nachun <daniel.nachun@gmail.com>
2024-07-22 15:01:20 -0700

    add platforms (#1)
    * add platforms
    
    * enable platforms
    
    * enable platforms
    
    * enable platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,18 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-make
-make install prefix=${PREFIX}
+if [[ ${build_platform} != ${target_platform} ]]; then
+    make reposurgeon
+    mkdir -p ${PREFIX}/bin
+    install -m 755 repocutter ${PREFIX}/bin/repocutter
+    install -m 755 repomapper ${PREFIX}/bin/repomapper
+    install -m 755 reposurgeon ${PREFIX}/bin/reposurgeon
+    install -m 755 repotool ${PREFIX}/bin/repotool
+else
+    make
+    make install prefix=${PREFIX}
+fi
+
 go-licenses save ./cutter --save_path=license-files_repocutter --ignore github.com/termie/go-shutil
 go-licenses save ./mapper --save_path=license-files_repomapper --ignore github.com/termie/go-shutil
 go-licenses save ./surgeon --save_path=license-files_reposurgeon --ignore github.com/termie/go-shutil
